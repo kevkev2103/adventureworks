d18b2a3960238e609e7c6d8993ac7f36fdfb02c0
kevin <kevin.pnathan@gmail.com>
2024-11-26 13:59:04 +0100

    téléchargement csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import pyodbc
+import pandas as pd
 
 load_dotenv()
 
@@ -17,14 +18,42 @@ connection_string = f'Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:{serve
 #Connexion à la base de données
 conn = pyodbc.connect(connection_string)
 
-#Exemple d'utilisation
+
+# création du dossier pour les CSV
+os.makedirs("csv", exist_ok = True)
+
+
+# Requête pour récupérer les noms des tables des schémas Person, Sales et Production
+
+schema_query = """
+SELECT TABLE_NAME, TABLE_SCHEMA
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA IN ('Person', 'Sales', 'Production') 
+  AND TABLE_TYPE = 'BASE TABLE';
+"""
+
+#Exécuter la requête pour récupérer les noms des tables
+
 cursor = conn.cursor()
-cursor.execute("SELECT TOP 10 * FROM Person.Person")
-rows = cursor.fetchall()
+cursor.execute(schema_query)
+tables = cursor.fetchall()
 
-for row in rows:
-    print(row)
+# Parcourir chaque table et sauvegarder son contenu dans un CSV
 
+for table_name,schema_name in tables:
+    try:
+        print(f"Extraction de la table : {schema_name}.{table_name}")
+        # Charger les données de la table dans un Dataframe Pandas
+        query = f"SELECT * FROM {schema_name}.{table_name};"
+        df = pd.read_sql(query, conn)
+
+        # Sauvegarder le Dataframe dans un fichier CSV
+        csv_filename = f"csv/{schema_name}_{table_name}.csv"
+        df.to_csv(csv_filename, index=False, encoding ='utf-8')
+        print(f"Table {schema_name}.{table_name} sauvegardée dans {csv_filename}.")
+    except Exception as e:
+        print(f"Erreur lors de l'extraction de {schema_name}.{table_name} : {e}")
 #Fermer
 cursor.close()
 conn.close()
+
